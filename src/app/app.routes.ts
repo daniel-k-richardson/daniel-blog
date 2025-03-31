91ef5bc6edfc77238c1cc944a48c9d5ea564bc97
Daniel Richardson <daniel@vigilo.no>
2025-03-31 14:52:39 +0200

    blog and css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: 'blogs',
+    loadComponent: () => import('./blogs/blogs.component').then(m => m.BlogsComponent)
+  },
+  {
+    path: 'blog/:id',
+    loadComponent: () => import('./blog/blog.component').then(m => m.BlogComponent)
+  },
+];
